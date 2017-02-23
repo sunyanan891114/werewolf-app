25601785d341866255710ac834315ccb56502235
sun.yanan <sun.yanan@rea-group.com>
2017-02-23 20:24:21 +0800

    Fix prophet page

--- src/js/GamePage.js
+++ src/js/GamePage.js
@@ -20,13 +20,15 @@ export default class GamePage extends Component {
     return (
       <div className="game_page__container">
         { this.renderVideoMessage()}
-        <span className="game_page__role" onClick={this.showOrHideRole}>{this.state.showLabel ? roleName[this.props.response.role] : "角色卡"}</span>
+        <span className="game_page__role"
+              onClick={this.showOrHideRole}>{this.state.showLabel ? roleName[this.props.response.role] : "角色卡"}</span>
         <div className="game_page__gameProcess">{ this.state.gameProcess }
           {!this.state.isReady && <button className="game_page-ready__button" onClick={this.readyForGame}>准备好了</button>}
+          { this.renderCommandSection() }
         </div>
-        { this.renderCommandSection() }
-        <i className={"game_page__avatar game_page__avatar-" + (this.state.showLabel?this.props.response.role:"random")} onClick={this.showOrHideRole}/>
-        { dispatchRole({sendAction: this.sendAction, response: this.props.response}) }
+        <i
+          className={"game_page__avatar game_page__avatar-" + (this.state.showLabel ? this.props.response.role : "random")}
+          onClick={this.showOrHideRole}/>
       </div>
     )
   }
@@ -39,17 +41,19 @@ export default class GamePage extends Component {
 
   renderVideoMessage() {
     return this.props.response.voice ?
-      <video controls autoplay name="media" style="display:none">
-        <source src={ baiduAudio(this.state.gameProcess) } type="dio/mp3"/>
-      </video> : null
+      <audio controls autoPlay name="media" style={{display: "none"}}>
+        <source src={ baiduAudio(this.state.gameProcess) } type="audio/mp3"/>
+      </audio> : null
   }
 
   renderCommandSection() {
-    return this.props.response.daylight ?
-      <div>
-        <input type="text" ref="number" placeholder="请输入投票的号码"/>
-        <button onClick={this.submit}>确定</button>
-      </div> : dispatchRole({sendAction: this.sendAction, response: this.props.response});
+    if (this.state.isReady) {
+      return (this.props.response.daylight ?
+        <div>
+          <input type="text" ref="number" placeholder="请输入投票的号码"/>
+          <button onClick={this.submit}>确定</button>
+        </div> : dispatchRole({sendAction: this.sendAction, response: this.props.response}));
+    }
   }
 
   submit = () => {
--- src/js/RolePage/ProphetPage.js
+++ src/js/RolePage/ProphetPage.js
@@ -6,13 +6,13 @@ export default class ProphetPage extends Component {
   }
 
   render() {
-    return <div>
-      <input type="text" placeholder="请输入你想要查看的号码" ref="predictNumber"/>
-      <button onClick={this.sendAction.bind(this, "predict", this.refs.predictNumber.value, true)}>确定</button>
+    return <div className="role-page">
+      <input type="text" placeholder="请输入你想要查看的号码" ref="predictNumber" className="role-page__input"/>
+      <button onClick={this.sendAction } className="role-page__confirm-btn">确定</button>
     </div>
   }
 
-  sendAction = (action, target) => {
-    this.props.sendAction(this.props.response.role + ":" + action, target);
+  sendAction = () => {
+    this.props.sendAction(this.props.response.role + ":" + "predict", this.refs.predictNumber.value);
   };
 }
