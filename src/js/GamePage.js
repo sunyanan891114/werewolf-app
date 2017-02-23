6c88e6340d02a9be455ad7c908261c4a35dc0e70
wenisy <wenisy@gmail.com>
2017-02-23 10:57:36 +0800

    Use the same JSON format with backend

--- src/js/GamePage.js
+++ src/js/GamePage.js
@@ -7,18 +7,22 @@ export default class GamePage extends Component {
     super(props);
     this.state = {
       role: "witch",
-      roleStatus: false,
-      action: true
+      alive: true,
+      daylight: true,
+      message: "",
+      voice: false,
+      showLabel: false,
+      skillStatus: {}
     }
   }
 
   render() {
     return (
       <div>
-        <span>角色:</span><span>{this.state.roleStatus ? this.state.role : "***"}</span>
-        <button onClick={this.showOrHideRole}>{this.state.action ? "查看" : "隐藏"}</button>
+        <span>角色:</span><span>{this.state.showLabel ? this.state.role : "***"}</span>
+        <button onClick={this.showOrHideRole}>{this.state.showLabel ? "隐藏" : "查看"}</button>
         <div>
-          { dispatchRole({sendAction: this.sendAction, status:true, role: this.state.role, deadNumber:"11"}) }
+          { dispatchRole({sendAction: this.sendAction, status: true, role: this.state.role, deadNumber: "11"}) }
           <input type="text" ref="number" placeholder="请输入投票的号码"/>
           <button onClick={this.submit}>确定</button>
         </div>
@@ -36,8 +40,7 @@ export default class GamePage extends Component {
 
   showOrHideRole = () => {
     this.setState({
-      roleStatus: !this.state.roleStatus,
-      action: !this.state.action
+      showLabel: !this.state.showLabel
     })
   }
 }
