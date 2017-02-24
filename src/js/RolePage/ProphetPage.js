892e24c7fdfa1392d1ff34e8c70abefedba75446
QiannanXu <qnxu@thoughtworks.com>
2017-02-24 14:09:06 +0800

    Fix Prophet page

--- src/js/RolePage/ProphetPage.js
+++ src/js/RolePage/ProphetPage.js
@@ -8,11 +8,11 @@ export default class ProphetPage extends Component {
   render() {
     return <div>
       <input type="text" placeholder="请输入你想要查看的号码" ref="predictNumber"/>
-      <button onClick={this.sendAction.bind(this, "predict", this.refs.predictNumber.value, true)}>确定</button>
+      <button onClick={this.sendAction}>确定</button>
     </div>
   }
 
-  sendAction = (action, target) => {
-    this.props.sendAction(this.props.response.role + ":" + action, target);
-  };
+  sendAction = () => {
+    this.props.sendAction("check", this.refs.predictNumber.value);
+  }
 }
