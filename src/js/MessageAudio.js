d65b5892db36fc922efebe88a83be40aa526ae82
wenisy <wenisy@gmail.com>
2017-02-24 10:47:42 +0800

    Add iphone safari autoplay and rerender audio src if it has updated

--- src/js/MessageAudio.js
+++ src/js/MessageAudio.js
@@ -10,7 +10,7 @@ export default class MessageAudio extends Component {
   }
 
   render() {
-    return this.props.voice ? this.renderAudioTag() : null;
+    return this.props.voice? this.renderAudioTag() : null;
   }
 
   componentWillReceiveProps(nextProps) {
@@ -19,20 +19,36 @@ export default class MessageAudio extends Component {
     });
   }
 
+  playAudio=()=>{
+    return document.querySelector('.audio-message').play();
+  };
+
+  componentDidMount(){
+    document.addEventListener('touchstart', () => {
+      this.playAudio();
+    }, false);
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener('touchstart', () => {
+      this.playAudio();
+    }, false);
+  }
+
   shouldComponentUpdate(nextProps) {
     return this.props.gameProcess !== nextProps.gameProcess;
   }
 
   componentDidUpdate() {
     if (this.props.voice) {
-      document.querySelector('.audio-message').play();
+      this.refs.audio.load();
     }
   }
 
   renderAudioTag() {
     return (
-      <audio controls autoPlay name="media" style={{display: "none"}} className="audio-message">
-        <source src={ baiduAudio(this.state.gameProcess) } type="audio/mp3"/>
+      <audio controls autoPlay name="media" style={{display: "none"}} className="audio-message" ref="audio">
+        <source key={Date.parse(new Date())} src={ baiduAudio(this.state.gameProcess) } type="audio/mp3"/>
       </audio>
     )
   }
